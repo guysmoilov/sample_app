57bcf632bd5f3bb212cf2ba8ccf4c68f78bac194
Guy Smoilov <guy.smoilov@gmail.com>
2011-02-15 10:40:23 +0200

    Added title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  
+  # Returns a default title if there's no specific one
+  def title
+    if @title.nil?
+      return "Sasi"
+    else
+      return "Sasi | #{@title}"
+    end
+  end
 end
